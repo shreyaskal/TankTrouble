d673e50a2b0a50a96ae39663b2350faa4329454d
shreyaskal <shreyaskal@gmail.com>
2014-03-08 22:21:21 -0800

    Newest1021pm

--- src/com/example/tanktrouble/TankTroubleActivity.java
+++ src/com/example/tanktrouble/TankTroubleActivity.java
@@ -3,37 +3,61 @@ package com.example.tanktrouble;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.RepeatingSpriteBackground;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.ITexture;
+import org.andengine.opengl.texture.atlas.bitmap.source.AssetBitmapTextureAtlasSource;
 import org.andengine.opengl.texture.bitmap.BitmapTexture;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TextureRegionFactory;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.adt.io.in.IInputStreamOpener;
 
-import android.app.Activity;
+import android.graphics.*;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.util.DisplayMetrics;
+import android.view.*;
 
-public class TankTroubleActivity extends SimpleBaseGameActivity{
+public class TankTroubleActivity extends SimpleBaseGameActivity implements IOnSceneTouchListener{
+
 
 	
 
-	private static int CAMERA_WIDTH = 800;
-	private static int CAMERA_HEIGHT = 480;
+
 	private ITextureRegion tank1, tankTurret1, tank2, tankTurret2;
+
+	private Tank tankOne;
+	private Tank tankTwo;
+	private TankTurret tankTurretOne;
+	private TankTurret tankTurretTwo;
+	private int CAMERA_WIDTH;
+	private int CAMERA_HEIGHT;
+	
+	
 	
 	
 	public EngineOptions onCreateEngineOptions() {
+		
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+		CAMERA_WIDTH = metrics.widthPixels;
+		CAMERA_HEIGHT = metrics.heightPixels;
+		
 		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 		return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, 
 		    new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
 
 	}
-	 
-	protected void onCreateResources() {
+
+	protected void onCreateResources() {	
 		try {
 			ITexture redTank = new BitmapTexture(this.getTextureManager(), new IInputStreamOpener() {
 			    @Override
@@ -59,49 +83,72 @@ public class TankTroubleActivity extends SimpleBaseGameActivity{
 			        return getAssets().open("images/TankTurret.png");
 			    }
 			});
+
+
 			
-			
-			
+
 			redTank.load();
 			blueTank.load();
 			redTankTurret.load();
 			blueTankTurret.load();
-			
+
 			this.tank1 = TextureRegionFactory.extractFromTexture(redTank);
 			this.tank2 = TextureRegionFactory.extractFromTexture(blueTank);
 			this.tankTurret1 = TextureRegionFactory.extractFromTexture(redTankTurret);
 			this.tankTurret2 = TextureRegionFactory.extractFromTexture(blueTankTurret);
-			
 
-			
+
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	
+
 	}
-	 
+
+
+	
+	
 	protected Scene onCreateScene() {
 		final Scene scene = new Scene();
-		Tank tankOne = new Tank(20, 20, tank1, getVertexBufferObjectManager(), 5);
-		Tank tankTwo = new Tank(20, 100, tank2, getVertexBufferObjectManager(), 5);
+		tankOne = new Tank(20, 20, tank1, getVertexBufferObjectManager(), 5);
+		tankTwo = new Tank(20, 100, tank2, getVertexBufferObjectManager(), 5);
 		float t1x = tankOne.getX();
 		float t1y = tankOne.getY();
 		float t2x = tankTwo.getX();
 		float t2y = tankTwo.getY();
+
+		tankTurretOne = new TankTurret(t1x - 5, t1y - 3, tankTurret1, getVertexBufferObjectManager(), 5);
+		tankTurretTwo = new TankTurret(t2x - 11, t2y - 10, tankTurret2, getVertexBufferObjectManager(), 5);
+
+		scene.setBackground(new RepeatingSpriteBackground(CAMERA_WIDTH,CAMERA_HEIGHT,getTextureManager(), 
+				AssetBitmapTextureAtlasSource.create(getAssets(), "images/Background.png"),getVertexBufferObjectManager()));
 		
-		TankTurret tankTurretOne = new TankTurret(t1x - 5, t1y - 3, tankTurret1, getVertexBufferObjectManager(), 5);
-		TankTurret tankTurretTwo = new TankTurret(t2x - 11, t2y - 10, tankTurret2, getVertexBufferObjectManager(), 5);
 		
 		scene.attachChild(tankOne);
 		scene.attachChild(tankTwo);
 		scene.attachChild(tankTurretOne);
 		scene.attachChild(tankTurretTwo);
+
+		scene.setOnSceneTouchListener(this);
 		
 		return scene;
 	}
 
 
+	public boolean onAreaTouched(TouchEvent t){
+		tankOne.setPosition(t.getX(), t.getY());
+		tankTurretOne.setPosition(tankOne.getX() - 5, tankTwo.getY() - 3);
+		return true;
+	}
 
-	
-	
+
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		
+		tankOne.setPosition(pSceneTouchEvent.getMotionEvent().getX(), pSceneTouchEvent.getMotionEvent().getY());
+		
+		return true;
+	}
+
+
 }
--- src/com/example/tanktrouble/TankTroubleMenu.java
+++ src/com/example/tanktrouble/TankTroubleMenu.java
@@ -19,23 +19,25 @@ import com.example.tanktrouble.util.SystemUiHider;
  * @see SystemUiHider
  */
 public class TankTroubleMenu extends Activity {
+
+	
 	/**
 	 * Whether or not the system UI should be auto-hidden after
 	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
 	 */
-	private static final boolean AUTO_HIDE = true;
+	private static final boolean AUTO_HIDE = false;
 
 	/**
 	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
 	 * user interaction before hiding the system UI.
 	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+	private static final int AUTO_HIDE_DELAY_MILLIS = 0; //used to be 3000
 
 	/**
 	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
 	 * will show the system UI visibility upon interaction.
 	 */
-	private static final boolean TOGGLE_ON_CLICK = true;
+	private static final boolean TOGGLE_ON_CLICK = false;
 
 	/**
 	 * The flags to pass to {@link SystemUiHider#getInstance}.
@@ -46,7 +48,6 @@ public class TankTroubleMenu extends Activity {
 	 * The instance of the {@link SystemUiHider} for this activity.
 	 */
 	private SystemUiHider mSystemUiHider;
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
